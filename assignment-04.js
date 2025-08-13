e406331c7fcbd432b231a955cae662f64b6bc51e
aktar86 <amdaktar86@gmail.com>
2025-08-13 22:00:36 +0600

    added three

--- assignment-04.js
+++ assignment-04.js
@@ -24,5 +24,29 @@ function  onlyCharacter( str ) {
     return character.toUpperCase();
 
 }
-const getCleanStr = onlyCharacter("  h e llo wor   ld")
-console.log(getCleanStr)
+// const getCleanStr = onlyCharacter("  h e llo wor   ld")
+// console.log(getCleanStr)
+
+function  bestTeam( player1, player2 ) {
+    let score1 = player1.foul + player1.cardY + player1.cardR;
+    let score2 = player2.foul + player2.cardY + player2.cardR;
+
+    if(typeof player1 !== "object" || typeof player2 !== "object"){
+        return "Invalid";
+    }
+
+    if(score1 === score2){
+        return "Tie";
+    }
+    else if(score1 > score2){
+        return player2.name;
+    }
+    else{
+        return player1.name;
+    }
+}
+
+// const getBestTeam = bestTeam({ name: "Brazil", foul: 5, cardY: 1, cardR: 0 },
+// { name: "Argentina", foul: 7, cardY: 0, cardR: 0 })
+// console.log(getBestTeam)
+
